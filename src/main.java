d56dab0bfc71075ad02af48758493e3892561ce3
Kat2 <katysombra@gmail.com>
2014-10-13 15:13:58 -0300

    Subindo alteração

--- src/main.java
+++ src/main.java
@@ -1,10 +1,17 @@
 
-public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Word");
+	public class main {
 
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+		
+			String name = "eclipse";
+			for (int i = 1; i < 100; i = i + 2) {
+				System.out.println("Contador!");
+
+			}
+		}
 	}
+	
 
-}
+
