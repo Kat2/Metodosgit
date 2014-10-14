a791f4c7e6212586786133058b9510105e227289
Kat2 <katysombra@gmail.com>
2014-10-14 16:29:24 -0300

    Commit inicial

---
+++ src/ContaTeste.java
@@ -0,0 +1,17 @@
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ContaTeste {
+
+	@Test
+	public void test() {
+		int num = 5;
+		String num2 = "";
+		Contador c = new Contador();
+		num2 = c.ContaPares(num);
+		assertEquals("números pares", num2);
+	}
+
+}
--- src/main.java
+++ src/main.java
@@ -2,15 +2,12 @@
 
 	public class main {
 
+		
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-		
-			String name = "eclipse";
-			String jota = "null";
-			for (int i = 1; i < 100; i = i + 2) {
-				System.out.println("Contador!");
-
-			}
+			Contador c = new Contador();
+			int num = 100;
+			c.ContaPares(num);
 		}
 	}
 	
